9228149b99912eefe3b9d2e9007682441457db68
tdmdak <161836000+tdmdak@users.noreply.github.com>
2024-12-18 21:21:31 +0530

    Add Minimal API

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuerHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQuerHandler<in TQuery, TResponse>
+        : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
