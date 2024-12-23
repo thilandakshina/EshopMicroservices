044137ff9c7476df0c0a4da6c46a1f8132f81a94
T_dak <thilandakshina@gmail.com>
2024-12-23 21:06:57 +0530

    Add Basket.API Related Changes

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+public string UserName { get; set; } = default!;
+    public List<ShoppingCartItems> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+     
+    public ShoppingCart()
+    {
+    }
+}
