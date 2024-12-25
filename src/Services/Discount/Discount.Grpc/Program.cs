b4c8a0b2711e4f294b776926f0517c982df68651
T_dak <thilandakshina@gmail.com>
2024-12-25 14:35:09 +0530

    Add Discount.Grpc

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,25 @@
+
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+///using Discount.Grpc.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+
+var assembly = typeof(Program).Assembly;
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMigration();
+
+app.MapGrpcService<DiscountService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
